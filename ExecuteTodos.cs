176e783c490f15e03461e44e217c78eb630541a3
Brendon Mascarenhas <brendon.genssinger@gmail.com>
2020-03-16 06:13:51 -0300

    1012

--- ExecuteTodos.cs
+++ ExecuteTodos.cs
@@ -1,71 +1,88 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 namespace PraticasProgramacao2020
 {
     public class ExecuteTodos
     {
-        public static void Executar_1010()
+
+        public static void Executar(int pNumber)
         {
-            while(true)
+            var dic = new Dictionary<int,Action>();
+            dic.Add(1010,new Action(Executar_1010));
+            dic.Add(1009,new Action(Executar_1009));
+            dic.Add(1011,new Action(Executar_1011));
+            dic.Add(1012,new Action(Executar_1012));
+            if(dic.ContainsKey(pNumber))
             {
-                var primeirosDados = Obter().ToString().Split(' ');
-                var segundosDados = Obter().ToString().Split(' ');
-
-                var total1 = Convert.ToInt16(primeirosDados[1]) * Convert.ToDouble(primeirosDados[2]);
-                var total2 = Convert.ToInt16(segundosDados[1]) * Convert.ToDouble(segundosDados[2]);
-
-                var sumTotais = total1+total2;
-                System.Console.WriteLine("VALOR A PAGAR: R$ "+sumTotais.ToString("0.00"));
+                while (true)
+                {
+                    dic[pNumber]();
+                }
             }
+
+        }
+        private static void Executar_1010()
+        {
+        
+            var primeirosDados = Obter().ToString().Split(' ');
+            var segundosDados = Obter().ToString().Split(' ');
+
+            var total1 = Convert.ToInt16(primeirosDados[1]) * Convert.ToDouble(primeirosDados[2]);
+            var total2 = Convert.ToInt16(segundosDados[1]) * Convert.ToDouble(segundosDados[2]);
+
+            var sumTotais = total1+total2;
+            System.Console.WriteLine("VALOR A PAGAR: R$ "+sumTotais.ToString("0.00"));
+            
         }
         
-        public static void Executar_1009()
+        private static void Executar_1009()
         {
-            while (true)
-            {
-                var NOME = Obter().ToString();
-                var SALARIO = Convert.ToDouble(Obter());
-                var VENDAS = Convert.ToDouble(Obter());
-                double PORCENTAGEM = 15;
+            
+            var NOME = Obter().ToString();
+            var SALARIO = Convert.ToDouble(Obter());
+            var VENDAS = Convert.ToDouble(Obter());
+            double PORCENTAGEM = 15;
 
-                double TOTAL = (VENDAS*(PORCENTAGEM/100)+ SALARIO);
-                System.Console.WriteLine("TOTAL = R$ "+ TOTAL.ToString("0.00"));
-            }
+            double TOTAL = (VENDAS*(PORCENTAGEM/100)+ SALARIO);
+            System.Console.WriteLine("TOTAL = R$ "+ TOTAL.ToString("0.00"));
+            
         }
 
-        public static void Executar_1011()
+        private static void Executar_1011()
         {
-            while (true)
-            {
-                double R = Convert.ToDouble(Obter());
-                double pi = 3.14159;
+            
+            double R = Convert.ToDouble(Obter());
+            double pi = 3.14159;
 
-                var resultado = 4/3.0*pi * Math.Pow(R,3);
-                System.Console.WriteLine("VOLUME = "+resultado.ToString("0.000"));
-            }
+            var resultado = 4/3.0*pi * Math.Pow(R,3);
+            System.Console.WriteLine("VOLUME = "+resultado.ToString("0.000"));
+            
         }
 
-        public static void Executar_1012()
+        private static void Executar_1012()
         {
-            while (true)
-            {
-                var entrada = Convert.ToString(Obter()).Split(" "); 
+            System.Console.WriteLine($"{MethodInfo.GetCurrentMethod().Name}");
+            
+            var entrada = Convert.ToString(Console.ReadLine()); 
 
-                var triangulo = (asDouble(entrada[0]) * asDouble(entrada[2]))/2;
+            var _A = Convert.ToDecimal(entrada.Split(" ")[0]);
+            var _B = Convert.ToDecimal(entrada.Split(" ")[1]);
+            var _C = Convert.ToDecimal(entrada.Split(" ")[2]);
 
-                var areaCirculo = Math.Pow(asDouble(entrada[2]),2) * 3.14159;
+            var areaTriangulo = Convert.ToDecimal((( _A * _C )/2));
+            var areaCirculo = Convert.ToDecimal( ( _C * _C )  * 3.14159m);
+            var areaTrapezio = Convert.ToDecimal((_A + _B)/2 * _C );
+            var areaQuadrado = Convert.ToDecimal(Convert.ToDecimal(Math.Pow(Convert.ToDouble(_B),2)));
+            
 
-                var areaTrapezio = ((asDouble(entrada[0]) + asDouble(entrada[1]))*asDouble(entrada[2]))/2;
-
-                var areaQuadrado =  Math.Pow(asDouble(entrada[1]),2);
-
-                var areaRetangulo = asDouble(entrada[0]) * asDouble(entrada[1]);
-
-                System.Console.WriteLine("TRIANGULO: "+triangulo.ToString("0.000"));
-                System.Console.WriteLine("CIRCULO: "+areaCirculo.ToString("0.000"));
-                System.Console.WriteLine("TRAPEZIO: "+areaTrapezio.ToString("0.000"));
-                System.Console.WriteLine("QUADRADO: "+areaQuadrado.ToString("0.000"));
-                System.Console.WriteLine("RETANGULO: "+ areaRetangulo.ToString("0.000"));
-            }      
+            Console.WriteLine("TRIANGULO: "+areaTriangulo.ToString("0.000"));
+            Console.WriteLine("CIRCULO: "+areaCirculo.ToString("0.000"));
+            Console.WriteLine("TRAPEZIO: "+areaTrapezio.ToString("0.000"));
+            Console.WriteLine("QUADRADO: "+areaQuadrado.ToString("0.000"));
+            Console.WriteLine("RETANGULO: "+ (_A*_B).ToString("0.000"));
+            Console.ReadLine();
+             
         }
 
         public static object Obter()
--- Program.cs
+++ Program.cs
@@ -13,44 +13,11 @@ namespace PraticasProgramacao2020
             //_1006.Executar();
             //_1007.Executar();
             //_1008.Executar();
-            //ExecuteTodos.Executar_1010();
-            //ExecuteTodos.Executar_1011();
-            //ExecuteTodos.Executar_1012();
-
-                string[] entrada = Console.ReadLine().ToString().Split(" ",3); 
-                double A = Convert.ToDouble(entrada[0]);
-                double B = Convert.ToDouble(entrada[1]);
-                double C = Convert.ToDouble(entrada[2]);
-                double pi = 3.14159;
-                double areaTriangulo = ( A * C )/2;
-                double areaCirculo = (C*C)  * pi;
-                double areaTrapezio = (A + B)/2* C ;
-                double areaQuadrado =  Math.Pow(B,2);
-                double areaRetangulo = A * B;
-                System.Console.WriteLine("TRIANGULO: "+areaTriangulo.ToString("0.000"));
-                System.Console.WriteLine("CIRCULO: "+areaCirculo.ToString("0.000"));
-                System.Console.WriteLine("TRAPEZIO: "+areaTrapezio.ToString("0.000"));
-                System.Console.WriteLine("QUADRADO: "+areaQuadrado.ToString("0.000"));
-                System.Console.WriteLine("RETANGULO: "+ areaRetangulo.ToString("0.000"));
+            //ExecuteTodos.Executar(1010);
+            //ExecuteTodos.Executar(1011);
+            ExecuteTodos.Executar(1012);
 
                 
-                var lista = new List<Exception>();
-                    for (int i = 0; i < 13; i++)
-                    {
-                        try
-                        {
-                            // roda 
-                        }
-                        catch(Exception eXs)
-                        {   lista.Add(eXs);
-                        }         
-                    }
-                if(lista.Count > 0)
-                {
-                    System.Console.WriteLine(string.Join(",",lista.Select(pX=> pX.Message)) );
-                }
-            
-            Console.ReadLine();
         }
 
         public static object Obter()
